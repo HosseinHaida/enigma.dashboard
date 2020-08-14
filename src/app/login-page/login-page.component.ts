c5203517bf33036f240af15d7a1b290ebabc8ce0
Hossein Heidari <squanderness@gmail.com>
2020-08-14 10:24:24 +0430

    email pass given

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,29 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators, NgForm } from '@angular/forms';
-import { UserLogService } from '../shared/services/user-log.service';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators, NgForm } from "@angular/forms";
+import { UserLogService } from "../shared/services/user-log.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-login-page',
-  templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss']
+  selector: "app-login-page",
+  templateUrl: "./login-page.component.html",
+  styleUrls: ["./login-page.component.scss"],
 })
 export class LoginPageComponent implements OnInit {
   loginForm: FormGroup;
   loginStatusGifSource: string = null;
 
-  constructor(private userLogService: UserLogService, private router: Router) { }
+  constructor(private userLogService: UserLogService, private router: Router) {}
 
   ngOnInit() {
     if (this.userLogService.isAuthenticated()) {
-      this.router.navigate(['/home/games']);
+      this.router.navigate(["/home/games"]);
     }
     this.loginForm = new FormGroup({
-      email: new FormControl(null, Validators.required),
-      password: new FormControl(null, Validators.required)
+      email: new FormControl("squanderness@gmail.com", Validators.required),
+      password: new FormControl("google", Validators.required),
     });
-    this.userLogService.loginStatus.subscribe(status => {
-      this.loginStatusGifSource = 'assets/' + status + '.gif'
+    this.userLogService.loginStatus.subscribe((status) => {
+      this.loginStatusGifSource = "assets/" + status + ".gif";
     });
   }
 
