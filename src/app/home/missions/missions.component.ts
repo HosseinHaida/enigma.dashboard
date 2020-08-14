c64812a838194b9c3668ab20f76ecb81e7364774
Hossein Heidari <squanderness@gmail.com>
2020-08-14 11:32:36 +0430

    minor fixes

--- src/app/home/missions/missions.component.ts
+++ src/app/home/missions/missions.component.ts
@@ -1,36 +1,41 @@
-import { Router, ActivatedRoute } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from "@angular/router";
+import { Component, OnInit } from "@angular/core";
 
-import { Mission } from './../../shared/models/mission.model';
-import { MissionsService } from '../../shared/services/missions.service';
-import { faEye, faTrashAlt, faImage } from '@fortawesome/free-solid-svg-icons';
+import { Mission } from "./../../shared/models/mission.model";
+import { MissionsService } from "../../shared/services/missions.service";
+import {
+  faEye,
+  faTrashAlt,
+  faImage,
+  faAlignJustify,
+} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
-  selector: 'app-missions',
-  templateUrl: './missions.component.html',
-  styleUrls: ['./missions.component.scss']
+  selector: "app-missions",
+  templateUrl: "./missions.component.html",
+  styleUrls: ["./missions.component.scss"],
 })
 export class MissionsComponent implements OnInit {
-  faEye = faEye;
+  faEdit = faAlignJustify;
   faDelete = faTrashAlt;
   faImage = faImage;
 
   missions: Mission[];
-  tableHeaders: string[] = ['Manage', 'Script', 'Name', 'Image'];
+  tableHeaders: string[] = ["Manage", "Script", "Name", "Image"];
 
   // for the Filter Pipe
-  tableFilterTypes: string[] = ['', ''];
-  arrayItemKeys: string[] = ['script', 'name'];
+  tableFilterTypes: string[] = ["", ""];
+  arrayItemKeys: string[] = ["script", "name"];
 
   constructor(
     private missionsService: MissionsService,
     private router: Router,
     private route: ActivatedRoute
-  ) { }
+  ) {}
 
   ngOnInit() {
     this.missions = this.missionsService.getMissions();
-    this.missionsService.missionsUpdated.subscribe(updatedMissions => {
+    this.missionsService.missionsUpdated.subscribe((updatedMissions) => {
       this.missions = updatedMissions;
     });
   }
@@ -40,23 +45,23 @@ export class MissionsComponent implements OnInit {
   }
 
   genPropertyValuesOfMission(mission: Mission) {
-    return Object.keys(mission).map(key => {
+    return Object.keys(mission).map((key) => {
       return mission[key];
     });
   }
 
   onCreateNew() {
-    this.router.navigate(['edit'], { relativeTo: this.route });
+    this.router.navigate(["edit"], { relativeTo: this.route });
   }
 
   onMissionDelete(id: string) {
-    const answer = confirm('Are you sure?');
+    const answer = confirm("Are you sure?");
     if (answer) {
       this.missionsService.deleteMission(id);
     }
   }
 
   onMissionEdit(id: number) {
-    this.router.navigate(['edit', id], { relativeTo: this.route });
+    this.router.navigate(["edit", id], { relativeTo: this.route });
   }
 }
